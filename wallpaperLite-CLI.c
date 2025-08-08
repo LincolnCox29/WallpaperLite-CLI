a03a0a2bab51d9bbafd3f7a6ed490b611ed62068
LincolnCox29 <just.kcgo@gmail.com>
2025-08-08 23:24:37 +1000

    Added pause in full screen mode + code review

--- wallpaperLite-CLI.c
+++ wallpaperLite-CLI.c
@@ -49,6 +49,7 @@ typedef void* (*libvlc_media_new_path_t)(void*, const char*);
 typedef void* (*libvlc_media_player_new_from_media_t)(void*);
 typedef void (*libvlc_media_player_set_hwnd_t)(void*, void*);
 typedef void (*libvlc_media_player_play_t)(void*);
+typedef void (*libvlc_media_player_stop_t)(void*);
 typedef void (*libvlc_media_release_t)(void*);
 typedef void (*libvlc_media_player_release_t)(void*);
 typedef void (*libvlc_release_t)(void*);
@@ -62,7 +63,7 @@ typedef struct EnumWindowsProcParams
 
 typedef struct VLC
 {
-    char videoPath[260];
+    char videoPath[MAX_PATH];
     void* player;
     void* media;
     void* inst;
@@ -72,6 +73,7 @@ typedef struct VLC
     libvlc_media_player_new_from_media_t libvlc_media_player_new_from_media;
     libvlc_media_player_set_hwnd_t libvlc_media_player_set_hwnd;
     libvlc_media_player_play_t libvlc_media_player_play;
+    libvlc_media_player_stop_t libvlc_media_player_stop;
     libvlc_media_release_t libvlc_media_release;
     libvlc_media_player_release_t libvlc_media_player_release;
     libvlc_release_t libvlc_release;
@@ -127,6 +129,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
             cleanup();
             PostQuitMessage(0);
             return 0;
+        case WM_QUIT:
+            cleanup();
+            return 0;
         default:
             return DefWindowProc(hWnd, msg, wParam, lParam);
     }
@@ -152,12 +157,29 @@ BOOL CALLBACK EnumWindowsProc(HWND tophandle, LPARAM lParam)
     return TRUE;
 }
 
-inline void GetWorkerW(EnumWindowsProcParams* params)
+HWND GetWorkerW()
 {
+    EnumWindowsProcParams params =
+    {
+        getWindowsVersion(),
+        FindWindowA("Progman", NULL),
+        NULL
+    };
+
     // Create WorkerW via message 
-    SendMessageTimeoutA(params->progman, 0x052C, 0xD, 0x1, SMTO_NORMAL, 1000, NULL);
+    SendMessageTimeoutA(params.progman, 0x052C, 0xD, 0x1, SMTO_NORMAL, 1000, NULL);
+
     // Find WorkerW
-    EnumWindows(EnumWindowsProc, (LPARAM)params);
+    EnumWindows(EnumWindowsProc, (LPARAM)&params);
+    if (params.hWorkerW)
+    {
+        printf("WorkerW: 0x%p\n", params.hWorkerW);
+        return params.hWorkerW;
+    }
+    else
+    {
+        printf("WorkerW not found\n");
+    }
 }
 
 void libvlcLoad()
@@ -188,6 +210,7 @@ void libvlcLoad()
     GET_FUNC(libvlc_media_player_new_from_media);
     GET_FUNC(libvlc_media_player_set_hwnd);
     GET_FUNC(libvlc_media_player_play);
+    GET_FUNC(libvlc_media_player_stop);
     GET_FUNC(libvlc_media_release);
     GET_FUNC(libvlc_media_player_release);
     GET_FUNC(libvlc_release);
@@ -195,9 +218,70 @@ void libvlcLoad()
     #undef GET_FUNC
 }
 
-int main(int argc, char* argv[])
+BOOL isActiveWindowFullScreen()
 {
-    // Parse command line arguments 
+    // Get active window
+    HWND hwnd = GetForegroundWindow();
+    if (!hwnd || !IsWindowVisible(hwnd))
+        return FALSE;
+
+    // Rule out desktop
+    char className[256];
+    GetClassNameA(hwnd, className, sizeof(className));
+    if (strcmp(className, "Progman") == 0 || strcmp(className, "WorkerW") == 0)
+        return FALSE;
+
+    // Get monitor from active window hwnd 
+    HMONITOR hMonitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+    if (!hMonitor)
+        return FALSE;
+
+    // Get monitor info (size)
+    MONITORINFO monitorInfo = { sizeof(monitorInfo) };
+    if (!GetMonitorInfo(hMonitor, &monitorInfo))
+        return FALSE;
+
+    // Get window size
+    RECT windowRect;
+    if (!GetWindowRect(hwnd, &windowRect))
+        return FALSE;
+
+    // CMP size
+    return (windowRect.left == monitorInfo.rcMonitor.left &&
+        windowRect.top == monitorInfo.rcMonitor.top &&
+        windowRect.right == monitorInfo.rcMonitor.right &&
+        windowRect.bottom == monitorInfo.rcMonitor.bottom);
+}
+
+void setPlayerState(BOOL* wasFullscreen)
+{
+    BOOL isFullscreen = isActiveWindowFullScreen();
+
+    if (isFullscreen != *wasFullscreen)
+    {
+        if (isFullscreen)
+        {
+            vlc.libvlc_media_player_stop(vlc.player);
+            Sleep(50);
+            #ifdef DEBUG
+            printf("Stop playing wallpaper\n");
+            #endif // DEBUG
+
+        }
+        else 
+        {
+            vlc.libvlc_media_player_play(vlc.player);
+            Sleep(50);
+            #ifdef DEBUG
+            printf("Playing wallpaper again\n");
+            #endif // DEBUG
+        }
+        *wasFullscreen = isFullscreen;
+    }
+}
+
+inline void parseCommandLineArguments(int argc, char** argv)
+{
     if (!argv[1] || argc < 1)
     {
         printf(
@@ -206,26 +290,27 @@ int main(int argc, char* argv[])
         );
         exit(1);
     }
-    sscanf_s(argv[1], "%s", vlc.videoPath, 260);
+    strncpy_s(vlc.videoPath, sizeof(vlc.videoPath), argv[1], _TRUNCATE);
+}
 
-    EnumWindowsProcParams params =
-    {
-        getWindowsVersion(),
-        FindWindowA("Progman", NULL),
-        NULL
-    };
+int main(int argc, char* argv[])
+{
+    parseCommandLineArguments(argc, argv);
 
-    // Get WorkerW HWND
-    GetWorkerW(&params);
-    if (params.hWorkerW) 
-        printf("WorkerW: 0x%p\n", params.hWorkerW);
-    else
-        printf("WorkerW not found\n");
+    HWND hWorkerW = GetWorkerW();
 
     libvlcLoad();
 
-    //Init VLC with: Disables audio, Suppressing non-error messages, hw-accelerated, looping
-    const char* VLCargs[] = {"--no-audio", "--quiet", "--avcodec-hw=dxva2", "--input-repeat=65535" };
+    //Init VLC with args
+    const char* VLCargs[] = 
+    {
+        "--no-audio",                   // Disables audio
+        "--quiet",                      // Suppressing non-error messages
+        "--avcodec-hw=dxva2",           // HW-accelerated
+        "--input-repeat=65535",         // Looping
+        "--drop-late-frames",           // Dropped frames due to lack of resources
+        "--avcodec-fast"                // Enable optimized decoding
+    };
     vlc.inst = vlc.libvlc_new(sizeof(VLCargs) / sizeof(VLCargs[0]), VLCargs);
     if (!vlc.inst)
     {
@@ -234,6 +319,7 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    // Init media
     vlc.media = vlc.libvlc_media_new_path(vlc.inst, vlc.videoPath);
     if (!vlc.media)
     {
@@ -242,6 +328,7 @@ int main(int argc, char* argv[])
         return 1;
     }
 
+    // Init player 
     vlc.player = vlc.libvlc_media_player_new_from_media(vlc.media);
     if (!vlc.player)
     {
@@ -251,17 +338,26 @@ int main(int argc, char* argv[])
     }
 
     // VLC hooking WorkerW
-    vlc.libvlc_media_player_set_hwnd(vlc.player, params.hWorkerW);
-    printf("Video attached to window 0x%p\n", params.hWorkerW);
+    vlc.libvlc_media_player_set_hwnd(vlc.player, hWorkerW);
+    printf("Video attached to window 0x%p\n", hWorkerW);
 
+    // Play
     vlc.libvlc_media_player_play(vlc.player);
 
     // Main loop
+    BOOL wasFullscreen = FALSE;
     MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0)) 
+    while (TRUE)
     {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        setPlayerState(&wasFullscreen);
+
+        Sleep(100);
     }
 
 	return 0;
